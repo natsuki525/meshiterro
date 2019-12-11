617825a0b8e4eadf378907975eb7aa8dd4b174d1
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-11 15:31:07 +0900

    [Add] トップページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8672d49cd06231af178611343bd91fa2e4c367832b05de59bcb1f39fb3fe6e300fd1c9d3c7ae4d8bd1cac9e3599a1d5a60c8d254ad18f52fb73c704c2b5ebc61'
